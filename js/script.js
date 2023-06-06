a65505034a980c0c5fd31e01d9c7cbd23266bdb9
AAdebusuyi <adeoladonald7@gmail.com>
2023-06-06 15:53:54 +0100

    stopping transitions

--- js/script.js
+++ js/script.js
@@ -22,3 +22,13 @@ function changeSearchIcon() {
   }
 }
 changeSearchIcon();
+
+// stopping all animation and transition
+let resizeTimer;
+Window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
